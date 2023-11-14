9ff6ccc4a81c743b2fc7e3c373db05d1082bdbe6
Rafael Leal Pardo <78731447+falilp@users.noreply.github.com>
2023-11-14 16:24:26 +0100

    Add files via upload

---
+++ es.uca.gii.csi23.cinderella.java/src/gui/FrmMain.java
@@ -0,0 +1,74 @@
+package gui;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.UIManager;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FrmMain {
+
+	private JFrame _frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmMain window = new FrmMain();
+					window._frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FrmMain() throws Exception{
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		_frame = new JFrame();
+		_frame.setBounds(100, 100, 800, 800);
+		_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		_frame.setJMenuBar(menuBar);
+		
+		JMenu mnuNew = new JMenu("Nuevo");
+		menuBar.add(mnuNew);
+		
+		JMenuItem mitNewTrabajador = new JMenuItem("Trabajador");
+		mitNewTrabajador.addActionListener(new ActionListener() { 
+			public void actionPerformed(ActionEvent e) { ShowInternalFrame(new IfrTrabajador(), 100, 100, 450, 300); } 
+		});
+		mnuNew.add(mitNewTrabajador);
+		
+		JMenu mnuSearch = new JMenu("Buscar");
+		menuBar.add(mnuSearch);
+		
+		JMenuItem mitSearchTabajador = new JMenuItem("Trabajador");
+		mnuSearch.add(mitSearchTabajador);
+		_frame.getContentPane().setLayout(null);
+	}
+
+	public void ShowInternalFrame(JInternalFrame ifr, int iX, int iY, int iWidth, int iHeight) {
+		ifr.setBounds(iX, iY, iWidth, iHeight);
+		_frame.getContentPane().add(ifr);
+		ifr.setVisible(true);
+	}
+}
---
+++ es.uca.gii.csi23.cinderella.java/src/gui/IfrTrabajador.java
@@ -0,0 +1,64 @@
+package gui;
+
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import data.Trabajador;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class IfrTrabajador extends JInternalFrame {
+	private Trabajador _trabajador = null;
+	private static final long serialVersionUID = 1L;
+	private JTextField txtName;
+	
+	/**
+	 * Create the frame.
+	 */
+	public IfrTrabajador() {
+		setClosable(true);
+		setResizable(true);
+		setTitle("Trabajador");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		
+		JLabel lblMessage = new JLabel("");
+		lblMessage.setForeground(Color.BLUE);
+		lblMessage.setBounds(10, 0, 414, 14);
+		getContentPane().add(lblMessage);
+		
+		JLabel lblName = new JLabel("Nombre");
+		lblName.setHorizontalAlignment(SwingConstants.CENTER);
+		lblName.setBounds(185, 83, 46, 14);
+		getContentPane().add(lblName);
+		
+		txtName = new JTextField();
+		txtName.setHorizontalAlignment(SwingConstants.CENTER);
+		txtName.setBounds(166, 108, 86, 20);
+		getContentPane().add(txtName);
+		txtName.setColumns(10);
+		
+		JButton butSave = new JButton("Guardar");
+		butSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if(_trabajador == null) _trabajador = new Trabajador(txtName.getText());
+					else _trabajador.SetNombre(txtName.getText());
+					
+					_trabajador.Save();
+				}
+				catch(Exception ex) {
+					JOptionPane.showMessageDialog(null, "Ha ocurrido un error: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		butSave.setBounds(166, 162, 89, 23);
+		getContentPane().add(butSave);
+
+	}
+}
